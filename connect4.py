010fadddc80a23ac90839bb43880637317553113
Riyan Ahmed <riyan.ahmed@outlook.com>
2021-05-01 04:08:45 -0400

    adding class to Board, writing reaction and update board code
    -connect 4 Board Class fully working
    -rough code for on_reaction_add, update_board, some parts might work others might not work, real rough, needs to fix bugs.

--- connect4.py
+++ connect4.py
@@ -9,61 +9,97 @@ board_piece = {'*': ':blue_circle: ', 'R': ':red_circle: ',
                'Y': ':yellow_circle: '}
 
 
-def create_board():
-    arr = [['*']*7]*6
+class Board:
+    """A recursive tree data structure.
+
+    Note the relationship between this class and RecursiveList; the only major
+    difference is that _rest has been replaced by _subtrees to handle multiple
+    recursive sub-parts.
+    """
+    # === Private Attributes ===
+    # The array representing the game board.
+    _arr: List[str]
+
+    # === Representation Invariants ===
+    # - If arr is None then self._arr is an empty 7x6 board.
+
+    # Methods
+    def __init__(self) -> None:
+        """Initialize a Board with empty (blue) circles.
+        self._arr is an empty 7x6 board.
+        >>> b = Board()
+        >>> b._arr
+        [['*', '*', '*', '*','*', '*', '*'],
+        ['*', '*', '*', '*','*', '*', '*'],
+        ['*', '*', '*', '*','*', '*', '*'],
+        ['*', '*', '*', '*','*', '*', '*'],
+        ['*', '*', '*', '*','*', '*', '*'],
+        ['*', '*', '*', '*','*', '*', '*'],]
+        """
+        arr = _create_board()
+        self._arr = arr
+
+    def empty_board(self):
+        for i in range(len(self._arr)):
+            self._arr[i] = "*"
+
+    def print_board(self) -> str:
+        msg = ''
+        for row in self._arr:
+            for item in row:
+                msg += board_piece[item]
+            msg += '\n'
+        return msg
+
+    def drop_piece(self, r: int, c: int, piece):
+        self._arr[r][c] = piece
+
+    def is_valid_location(self, col):
+        if self._arr[col] == '*':
+            return True
+        return False
+
+    # for piece can use i from other function
+    def winning_move(self, piece) -> bool:
+        for c in range(COL_COUNT):
+            for r in range(ROW_COUNT):
+                if c <= COL_COUNT - 3:
+                    # Horizontal Wins
+                    if (self._arr[r][c] == self._arr[r][c + 1] == self._arr[r][
+                        c + 2]
+                            == self._arr[r][c + 3] == piece):
+                        return True
+
+                if r <= ROW_COUNT - 3:
+                    # Vertical Wins
+                    if self._arr[r][c] == self._arr[r + 1][c] == \
+                            self._arr[r + 2][c] == \
+                            self._arr[r + 3][c] == piece:
+                        return True
+
+                # Positive Diagonal Wins
+                if c <= COL_COUNT - 3 and r <= ROW_COUNT - 3:
+                    if self._arr[r][c] == self._arr[r + 1][c + 1] == \
+                            self._arr[r + 2][c + 2] == \
+                            self._arr[r + 3][c + 3] == piece:
+                        return True
+
+                # Negative Diagonal Wins
+                if c <= COL_COUNT - 3 and 3 <= r < ROW_COUNT:
+                    if self._arr[r][c] == self._arr[r - 1][c + 1] == \
+                            self._arr[r - 2][c + 2] == \
+                            self._arr[r - 3][c + 3] == piece:
+                        return True
+        return False
+
+    def current_row(self) -> int:
+        """
+
+        :return: Current row the board is at, which is len of list.
+        """
+        return self._arr.__len__()
+
+# Helpers
+def _create_board():
+    arr = [['*'] * 7] * 6
     return arr
-
-
-# for piece can use i from other function
-def winning_move(board, piece) -> bool:
-
-    for c in range(COL_COUNT):
-        for r in range(ROW_COUNT):
-            if c <= COL_COUNT - 3:
-                # Horizontal Wins
-                if (board[r][c] == board[r][c+1] == board[r][c+2]
-                        == board[r][c+3] == piece):
-                    return True
-
-            if r <= ROW_COUNT - 3:
-                # Vertical Wins
-                if board[r][c] == board[r+1][c] == board[r+2][c] == \
-                        board[r+3][c] == piece:
-                    return True
-
-            # Positive Diagonal Wins
-            if c <= COL_COUNT - 3 and r <= ROW_COUNT - 3:
-                if board[r][c] == board[r+1][c+1] == board[r+2][c+2] == \
-                        board[r+3][c+3] == piece:
-                    return True
-
-            # Negative Diagonal Wins
-            if c <= COL_COUNT - 3 and 3 <= r < ROW_COUNT:
-                if board[r][c] == board[r-1][c+1] == board[r-2][c+2] == \
-                        board[r-3][c+3] == piece:
-                    return True
-    return False
-
-
-def empty_board(arr):
-    for i in range(len(arr)):
-        arr[i] = "*"
-
-
-def print_board(board: List[list]) -> str:
-    msg = ''
-    for row in board:
-        for item in row:
-            msg += board_piece[item]
-        msg += '\n'
-    return msg
-
-
-def drop_piece(arr, r, c, piece):
-    arr[r][c] = piece
-
-
-def is_valid_location(board, col):
-    if board[col] == '*':
-        return True
-    return False
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 from keep_alive import keep_alive
-from connect4 import print_board, create_board
+from connect4 import Board
+from typing import Optional
 
 load_dotenv('.env')
 
@@ -35,13 +36,54 @@ async def help(ctx):
 async def play(ctx):
     player1 = ctx.author
     player2 = ctx.message.mentions[0]
-    board = create_board()
-    send_board = print_board(board)
+    board = Board()
+    send_board = board.print_board()
     message = await ctx.send(send_board)
-    for emoji in EMOTES:
-        await message.add_reaction(emoji)
+    # for emoji in EMOTES: idk what this does
+    #     await message.add_reaction(emoji)
+
+    reaction = await client.add_reaction()
+    c = await on_reaction_add(reaction, player1)
+    update_board(reaction, message, board, c)
+
+
+@client.event
+async def on_reaction_add(reaction, user) -> Optional[int]:
+    """
+    Check which reaction role was pressed from positions 0-5 on EMOTES.
+    :return: The corresponding number to that reaction, which is column <c>.
+    """
+    channel_id = 3123122  # ur channel id, not real.
+    if reaction.message.channel.id != channel_id:
+        return
+    if reaction.emoji == EMOTES[0]:
+        return 0
+    elif reaction.emoji == EMOTES[1]:
+        return 1
+    elif reaction.emoji == EMOTES[2]:
+        return 2
+    elif reaction.emoji == EMOTES[3]:
+        return 3
+    elif reaction.emoji == EMOTES[4]:
+        return 4
+    elif reaction.emoji == EMOTES[5]:
+        return 5
+
+
+def update_board(reaction, message, curr_board: Board, c):
+    # React to emoji reactions
+    # Edit Discord Messages
+
+    # create a new board with the new piece in it.
+    # new_board = Board()
+    # new_board.drop_piece()
+    r = curr_board.current_row()
+    # c corresponds to the emoji pressed.
+
+    curr_board.drop_piece(r, c, reaction)
+    await message.edit(content="the new content of the message")
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
 # client.close()
-
