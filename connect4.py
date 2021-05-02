07709c817c79c372add0e24cd8dc3b4d695082f1
pillscapsules <pirooz321@gmail.com>
2021-05-02 10:34:50 -0400

    started ai, changed _arr variable to _board

--- connect4.py
+++ connect4.py
@@ -10,22 +10,23 @@ class Board:
     """The board for the connect 4 game
     """
     # === Private Attributes ===
+    # _board:
     # The array representing the game board.
-    _arr: List[List[str]]
+    _board: List[List[str]]
 
     # Methods
     def __init__(self) -> None:
         """Initialize a Board with empty (blue) circles.
         self._arr is an empty 7x6 board.
         """
-        arr = _create_board()
-        self._arr = arr
+        arr = [['*'] * 7 for _ in range(6)]
+        self._board = arr
 
     def print_board(self) -> str:
         """Used to print the board with blue circles as empty areas,
         red circles as player 1 moves, and yellow circles as player 2 moves"""
         msg = ''
-        for row in self._arr:
+        for row in self._board:
             for item in row:
                 msg += board_piece[item]
             msg += '\n'
@@ -33,13 +34,13 @@ class Board:
 
     def is_valid_location(self, r: int, c: int):
         """Check if the section of the board is not already filled"""
-        if self._arr[r][c] == '*':
+        if self._board[r][c] == '*':
             return True
         return False
 
     def drop_piece(self, r: int, c: int, piece):
         """Drop piece of one of the players into the board"""
-        self._arr[r][c] = piece
+        self._board[r][c] = piece
 
     # for piece can use i from other function
     def winning_move(self, piece) -> bool:
@@ -49,38 +50,35 @@ class Board:
 
                 # Horizontal Wins
                 if c < COL_COUNT - 3:
-                    if (self._arr[r][c] == self._arr[r][c + 1] == self._arr[r][
-                        c + 2]
-                            == self._arr[r][c + 3] == piece):
+                    if (self._board[r][c] == self._board[r][c + 1] == self.
+                            _board[r][c + 2] == self._board[r][c + 3] == piece):
                         return True
 
                 # Vertical Wins
                 if r < ROW_COUNT - 3:
-                    if self._arr[r][c] == self._arr[r + 1][c] == \
-                            self._arr[r + 2][c] == \
-                            self._arr[r + 3][c] == piece:
+                    if self._board[r][c] == self._board[r + 1][c] == \
+                            self._board[r + 2][c] == \
+                            self._board[r + 3][c] == piece:
                         return True
 
                 # Positive Diagonal Wins
                 if c < COL_COUNT - 3 and r < ROW_COUNT - 3:
-                    if self._arr[r][c] == self._arr[r + 1][c + 1] == \
-                            self._arr[r + 2][c + 2] == \
-                            self._arr[r + 3][c + 3] == piece:
+                    if self._board[r][c] == self._board[r + 1][c + 1] == \
+                            self._board[r + 2][c + 2] == \
+                            self._board[r + 3][c + 3] == piece:
                         return True
 
                 # Negative Diagonal Wins
                 if c < COL_COUNT - 3 and 3 <= r < ROW_COUNT:
-                    if self._arr[r][c] == self._arr[r - 1][c + 1] == \
-                            self._arr[r - 2][c + 2] == \
-                            self._arr[r - 3][c + 3] == piece:
+                    if self._board[r][c] == self._board[r - 1][c + 1] == \
+                            self._board[r - 2][c + 2] == \
+                            self._board[r - 3][c + 3] == piece:
                         return True
         return False
 
-
-# Helpers
-def _create_board():
-    arr = [['*'] * 7 for _ in range(6)]
-    return arr
+    def score_position(self, piece):
+        for r in range(ROW_COUNT):
+            row_array = [int(i) for i in list(self._board[r, :])]
 
 
 if __name__ == '__main__':
