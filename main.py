b4bb70997e5bc8b41046d166a8c808d511eac1fe
pillscapsules <pirooz321@gmail.com>
2021-05-02 06:39:02 -0400

    added forfeit option

--- main.py
+++ main.py
@@ -15,21 +15,18 @@ load_dotenv('.env')
 client = commands.Bot(command_prefix='4', case_insensitive=True)
 client.remove_command('help')
 
-EMOTES = {'1️⃣': 0, '2️⃣': 1, '3️⃣': 2, '4️⃣': 3, '5️⃣': 4, '6️⃣': 5, '7️⃣': 6}
+EMOTES = {'1️⃣': 0, '2️⃣': 1, '3️⃣': 2, '4️⃣': 3, '5️⃣': 4, '6️⃣': 5, '7️⃣': 6,
+          '🏳': 'F'}
 TOP_NUM = '** **\n:one: :two: :three: :four: :five: :six: :seven: \n'
 IDS = {}
 P_DICT = {True: [1, 'R', discord.Colour.red()],
           False: [2, 'Y', discord.Colour.gold()]}
-GIFS = ['https://media1.tenor.com/images/514b6b9fe1e8afd9caa482275132d42e/'
-        'tenor.gif?itemid=19830879', 'https://media1.tenor.com/images/'
-        '7f2e03cad4e085c2cf9371bbcab98522/tenor.gif', 'https://'
-        'ftw.usatoday.com/wp-content/uploads/sites/90/2013/05/untitled-331.gif',
-        'https://media1.tenor.com/images/'
-        '1be1127ceaa51b001234009f26a56378/tenor.gif',
-        'https://thumbs.gfycat.com/AcademicKnobbyGrouse-max-1mb.gif', 'https://'
-        'media3.giphy.com/media/vmon3eAOp1WfK/giphy.gif?cid=790b7611056376e13a'
-        'f6146642ba87d9f5911db825664b24&rid=giphy.gif',
-        'https://i.makeagif.com/media/1-24-2016/OaJxDC.gif']
+GIFS = []
+gif_file = open("win_gifs.txt", "r")
+content = gif_file.readline()
+while content != '':
+    content = gif_file.readline()
+    GIFS.append(content)
 
 
 @client.event
@@ -57,10 +54,17 @@ async def play(ctx):
         await ctx.send(':x: ERROR: Someone is already playing in this channel')
         return None
     player1 = ctx.author
-    player2 = ctx.message.mentions[0]
+    try:
+        player2 = ctx.message.mentions[0]
+    except IndexError:
+        await ctx.send('Mention a person!')
+        return None
     board = Board()
-    message = await ctx.send(TOP_NUM + board.print_board() +
-                             f'\n Current player: <@{player1.id}>')
+    message = await ctx.send(f'{player1.display_name} :vs: '
+                             f'{player2.display_name} \n'
+                             + TOP_NUM + board.print_board() +
+                             f'\n Current player: <@{player1.id}>'
+                             f'\n :flag_white:: Forfeit')
     # idk what this does <- it reacts to the message with the seven emotes;
     # this is so that the players can click on the reactions rather than
     # having to add the reactions themselves
@@ -78,11 +82,20 @@ async def on_reaction_add(reaction, user) -> None:
     # If reaction is in a channel where no one is playing, or if reaction is
     # not a valid reaction, or if the person adding the reactions is the bot,
     # do nothing.
+    channel = client.get_channel(reaction.message.channel.id)
     if reaction.message.channel.id not in IDS or \
             user.id == 837837082948534272:
         return None
     elif reaction.emoji not in EMOTES.keys():
         await reaction.remove(user)
+        return None
+    elif EMOTES[reaction.emoji] == 'F':
+        del IDS[reaction.message.channel.id]
+        embed = discord.Embed(title=f'{user.display_name} forfeited!',
+                              color=discord.Colour.green())
+        embed.set_image(url='https://media1.tenor.com/images/'
+                            '8c3cb918305bf277589c6ad84dfcea53/tenor.gif')
+        await channel.send(embed=embed)
         return None
     curr_channel = IDS[reaction.message.channel.id]
     curr_piece = curr_channel[3]
@@ -91,11 +104,14 @@ async def on_reaction_add(reaction, user) -> None:
     curr_player = curr_channel[P_DICT[player_red][0]]
     other_player = curr_channel[P_DICT[not player_red][0]]
     await reaction.remove(user)
-    channel = client.get_channel(reaction.message.channel.id)
     if not curr_board.is_valid_location(0, EMOTES[reaction.emoji]):
-        await reaction.message.edit(content=TOP_NUM + curr_board.print_board() +
-                                    f':x: ERROR: Column full. :x:'
-                                    f'\n Current player: <@{curr_player.id}>')
+        await reaction.message.edit(content=f'{curr_channel[1].display_name} '
+                                            f':crossed_swords: '
+                                            f'{curr_channel[2].display_name} \n'
+                                            + TOP_NUM + curr_board.print_board()
+                                            + f':x: ERROR: Column full. :x:'
+                                    f'\n Current player: <@{curr_player.id}>'
+                                    f'\n :flag_white:: Forfeit')
         return None
     if user != curr_player:
         return None
@@ -104,15 +120,23 @@ async def on_reaction_add(reaction, user) -> None:
     while not curr_board.is_valid_location(r, EMOTES[reaction.emoji]):
         r -= 1
     curr_board.drop_piece(r, EMOTES[reaction.emoji], curr_piece)
-    await reaction.message.edit(content=TOP_NUM + curr_board.print_board() +
-                                f'\n Current player: <@{other_player.id}>')
+    await reaction.message.edit(content=f'{curr_channel[1].display_name} '
+                                        f':crossed_swords: '
+                                        f'{curr_channel[2].display_name} \n' +
+                                        TOP_NUM + curr_board.print_board() +
+                                f'\n Current player: <@{other_player.id}>'
+                                f'\n :flag_white:: Forfeit')
     if curr_board.winning_move(curr_piece):
-        player = str(curr_player)
         curr_color = P_DICT[player_red][2]
-        embed = discord.Embed(title=f'{player[:-5]} wins!',
+        embed = discord.Embed(title=f'{curr_player.display_name} wins!',
                               color=curr_color)
         embed.set_image(url=random.choice(GIFS))
         await channel.send(embed=embed)
+        await reaction.message.edit(content=f'{curr_channel[1].display_name}'
+                                            f' :crossed_swords: '
+                                            f'{curr_channel[2].display_name}\n'
+                                            + TOP_NUM + curr_board.print_board()
+                                            + f'\n<@{curr_player.id}> wins!')
         del IDS[reaction.message.channel.id]
 
 
